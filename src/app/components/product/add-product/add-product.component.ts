785edfaed7a88c3c1a3a20d1c30c48f7ebb6cc82
lina-22 <haquefahinur@gmail.com>
2024-04-14 23:59:10 +0200

    working on product add

--- src/app/components/product/add-product/add-product.component.ts
+++ src/app/components/product/add-product/add-product.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrl: './add-product.component.css'
+  styleUrl: './add-product.component.css',
 })
-export class AddProductComponent {
+export class AddProductComponent implements OnInit, OnDestroy {
+  constructor() {}
 
+  addProduct = new FormGroup({
+    productName: new FormControl(''),
+    productDescription: new FormControl(''),
+  });
+  ngOnInit(): void {}
+  ngOnDestroy(): void {}
 }
